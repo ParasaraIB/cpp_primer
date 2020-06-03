b118dd057557e75a4ca15c37ed8d818aa8b7f507
parasaraib <manikparasara@ymail.com>
2020-06-03 23:03:33 +0800

    init exercises 1.5.2

---
+++ 1/exercises1.5.2.cpp
@@ -0,0 +1,33 @@
+// // Exercise 1.23
+// #include <iostream>
+// #include "Sales_item.h"
+// int main()
+// {
+//     Sales_item item1, item2;
+//     if (std::cin >> item1) {
+//         int counter = 1;
+//         while (std::cin >> item2) {
+//             if (item1.isbn() == item2.isbn()) 
+//                 ++counter;
+//             else {
+//                 std::cout << item1.isbn() << " occurs " 
+//                           << counter << std::endl;
+//                 item1 = item2;
+//                 counter = 1;
+//             }    
+//         }
+//         std::cout << item1.isbn() << " occurs "
+//                   << counter << std::endl;
+//     }
+//     return 0;
+// }
+
+// // Exercise 1.24
+// /*
+//  * The program in Exercise 1.23 can be used to calculate the occurence
+//  * of each isbn in several transactions and provide grouped records for
+//  * each ISBN if and only if the inputted transaction given in a sequential
+//  * order. For example: aa 1 100 aa 1 100 aa 1 100 aa 1 100 bb 1 200 bb 34 200
+//  * yield: 
+//  * aa occurs 4
+//  * bb occurs 2
