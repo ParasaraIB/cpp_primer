8c490cdf7b1cf673d24802c79b623e3e5abfe537
parasaraib <manikparasara@ymail.com>
2020-06-03 12:36:51 +0800

    init exercise 1.4.3

--- 1/exercises1.4.3.cpp
+++ 1/exercises1.4.3.cpp
@@ -1,12 +1,12 @@
-// Exercise 1.16
-#include <iostream>
-int main()
-{
-    int sum = 0;
-    std::cout << "*press (Ctrl + d) when done\n" "Insert numbers: " 
-              << std::endl;
-    for (int val; std::cin >> val;)
-        sum += val;
-        std::cout << "Sum is " << sum << std::endl;
-    return 0;
-}
+// // Exercise 1.16
+// #include <iostream>
+// int main()
+// {
+//     int sum = 0;
+//     std::cout << "*press (Ctrl + d) when done\n" "Insert numbers: " 
+//               << std::endl;
+//     for (int val; std::cin >> val;)
+//         sum += val;
+//         std::cout << "Sum is " << sum << std::endl;
+//     return 0;
+// }
