249aa4405be69a8d223db944bcccd20a5666756f
parasaraib <manikparasara@ymail.com>
2020-06-03 23:43:29 +0800

    init exercises 1.6

---
+++ 1/exercises1.6.cpp
@@ -0,0 +1,23 @@
+// Exercise 1.25
+#include <iostream>
+#include "Sales_item.h"
+int main()
+{
+    Sales_item item1;
+    if (std::cin >> item1) {
+        Sales_item item2;
+        while (std::cin >> item2) {
+            if (item1.isbn() == item2.isbn())
+                item1 += item2;
+            else {
+                std::cout << item1 << std::endl;
+                item1 = item2;
+            }
+        }
+        std::cout << item1 << std::endl;
+    } else {
+        std::cerr << "No data?!" << std::endl;
+        return -1;
+    }
+    return 0;
+}
