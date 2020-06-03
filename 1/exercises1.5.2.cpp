422155462b2ad5a2efc703122a9dabd6bc8aa871
parasaraib <manikparasara@ymail.com>
2020-06-03 23:16:38 +0800

    fix solution

--- 1/exercises1.5.2.cpp
+++ 1/exercises1.5.2.cpp
@@ -1,26 +1,22 @@
-// // Exercise 1.23
-// #include <iostream>
-// #include "Sales_item.h"
-// int main()
-// {
-//     Sales_item item1, item2;
-//     if (std::cin >> item1) {
-//         int counter = 1;
-//         while (std::cin >> item2) {
-//             if (item1.isbn() == item2.isbn()) 
-//                 ++counter;
-//             else {
-//                 std::cout << item1.isbn() << " occurs " 
-//                           << counter << std::endl;
-//                 item1 = item2;
-//                 counter = 1;
-//             }    
-//         }
-//         std::cout << item1.isbn() << " occurs "
-//                   << counter << std::endl;
-//     }
-//     return 0;
-// }
+// Exercise 1.23
+#include <iostream>
+#include "Sales_item.h"
+int main()
+{
+    Sales_item item1, item2;
+    if (std::cin >> item1) {
+        while (std::cin >> item2) {
+            if (item1.isbn() == item2.isbn()) 
+                item1 += item2;
+            else {
+                std::cout << item1 << std::endl;
+                item1 = item2;
+            }    
+        }
+        std::cout << item1 << std::endl;
+    }
+    return 0;
+}
 
 // // Exercise 1.24
 // /*
@@ -29,5 +25,5 @@
 //  * each ISBN if and only if the inputted transaction given in a sequential
 //  * order. For example: aa 1 100 aa 1 100 aa 1 100 aa 1 100 bb 1 200 bb 34 200
 //  * yield: 
-//  * aa occurs 4
-//  * bb occurs 2
+//  * aa 4 400 100
+//  * bb 35 7000 200
