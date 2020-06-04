65e226485fcababffda18b0336e28e40768a2a50
parasaraib <manikparasara@ymail.com>
2020-06-04 20:47:35 +0800

    fix exercise 2.4

--- 2/exercises2.1.2.cpp
+++ 2/exercises2.1.2.cpp
@@ -10,17 +10,17 @@ unsigned u = 10, u2 = 42;
 [In] std::cout << u - i << std::endl;   // [Out] 0
 */
 
-// Exercise 2.4
-#include <iostream>
-int main()
-{
-    unsigned u = 10, u2 = 42;
-    std::cout << u2 - u << std::endl;
-    std::cout << u - u2 << std::endl;
-    int i = 10, i2 = 42;
-    std::cout << i2 - i << std::endl;
-    std::cout << i - i2 << std::endl;
-    std::cout << i - u << std::endl;
-    std::cout << u - i << std::endl;
-    return 0;
-}
+// // Exercise 2.4
+// #include <iostream>
+// int main()
+// {
+//     unsigned u = 10, u2 = 42;
+//     std::cout << u2 - u << std::endl;
+//     std::cout << u - u2 << std::endl;
+//     int i = 10, i2 = 42;
+//     std::cout << i2 - i << std::endl;
+//     std::cout << i - i2 << std::endl;
+//     std::cout << i - u << std::endl;
+//     std::cout << u - i << std::endl;
+//     return 0;
+// }
